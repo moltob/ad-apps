17a5ee3b8be4fc93df78edde423aa20403b87932
Mike Pagel <mike@mpagel.de>
2023-01-27 18:17:50 +0100

    reorganize code to enable sharing

--- radiator.py
+++ radiator.py
@@ -1,24 +1,9 @@
 """Basic radiator control through external temperature sensor."""
 import datetime
-import typing as t
 
-import appdaemon.plugins.hass.hassapi
+from util.base import MyHomeAssistantApp
 
 PERIOD_MINUTES = 15
-
-
-class MyHomeAssistantApp(appdaemon.plugins.hass.hassapi.Hass):
-    """Base class exposing some utilities for AppDaemon apps.
-
-    For convenience, some wrappers for base class methods are offered in an "ae" version, meaning
-    they take the entity ID as indirect reference by an app argument.
-    """
-
-    def get_state_ae(self, argument_name: str, *args, **kwargs) -> str:
-        return t.cast(str, self.get_state(self.args[argument_name], *args, **kwargs))
-
-    def call_service_ae(self, service: str, argument_name: str, **kwargs):
-        self.call_service(service, entity_id=self.args[argument_name], **kwargs)
 
 
 class RadiatorApp(MyHomeAssistantApp):
---
+++ util/base.py
@@ -0,0 +1,19 @@
+import appdaemon.plugins.hass.hassapi
+import typing as t
+
+
+PERIOD_MINUTES = 15
+
+
+class MyHomeAssistantApp(appdaemon.plugins.hass.hassapi.Hass):
+    """Base class exposing some utilities for AppDaemon apps.
+
+    For convenience, some wrappers for base class methods are offered in an "ae" version, meaning
+    they take the entity ID as indirect reference by an app argument.
+    """
+
+    def get_state_ae(self, argument_name: str, *args, **kwargs) -> str:
+        return t.cast(str, self.get_state(self.args[argument_name], *args, **kwargs))
+
+    def call_service_ae(self, service: str, argument_name: str, **kwargs):
+        self.call_service(service, entity_id=self.args[argument_name], **kwargs)
