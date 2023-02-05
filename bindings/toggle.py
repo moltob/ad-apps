41529d40a1dae9952162f6ef3e8fc36504f282c9
Mike Pagel <mike@mpagel.de>
2023-02-05 11:17:26 +0100

    allow overriding switch action
    Configure attic and dining room switches and lights. Disable info
    (nonexistent entities can no longer be created, irrelevant for now).

--- bindings/toggle.py
+++ bindings/toggle.py
@@ -4,7 +4,9 @@ import appdaemon.entity
 from util.base import MyHomeAssistantApp
 
 
-class ToggleApp(MyHomeAssistantApp):
+class ActionToggleApp(MyHomeAssistantApp):
+    """A toggle exposing an action."""
+
     ent_sensor: appdaemon.entity.Entity
     ent_actuator: appdaemon.entity.Entity
 
@@ -16,7 +18,9 @@ class ToggleApp(MyHomeAssistantApp):
             self.ent_sensor.entity_id,
             self.ent_actuator.entity_id,
         )
-        await self.ent_sensor.listen_state(self.toggle_actuator, new='single')
+        await self.ent_sensor.listen_state(
+            self.toggle_actuator, new=self.args.get('sensor_action', 'single')
+        )
 
     async def toggle_actuator(self, entity, attribute, old, new, kwargs):
         await self.ent_actuator.toggle()
