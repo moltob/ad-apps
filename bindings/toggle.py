751e8ed1e186cbee891bd3d56e9e303c359b320e
Mike Pagel <mike@mpagel.de>
2023-02-16 19:25:43 +0100

    add multi-light toggle

--- bindings/toggle.py
+++ bindings/toggle.py
@@ -53,3 +53,64 @@ class WelcomeLightApp(MyHomeAssistantApp):
         await self.ent_light.turn_on(effect='breathe')
         await self.sleep(3)
         await self.ent_light.turn_on(effect='finish_effect')
+
+
+class MultiLightToggleApp(MyHomeAssistantApp):
+    """Control and toggle multiple lights with a single/double action switch.
+
+    Single action toggles through the lights, turning on only one at a time. Double action will turn
+    on/off all lights at once.
+    """
+
+    ent_lights: list[appdaemon.entity.Entity]
+    ent_switch: appdaemon.entity.Entity
+
+    async def initialize(self):
+        await super().initialize()
+
+        self.logger.info(
+            'Binding switch %r to these lights: %s',
+            self.ent_switch.entity_id,
+            ', '.join(e.entity_id for e in self.ent_lights),
+        )
+
+        await self.ent_switch.listen_state(self.switch_to_next_light, new='single')
+        await self.ent_switch.listen_state(self.toggle_all_lights, new='double')
+
+    async def switch_to_next_light(self, entity, attribute, old, new, kwargs):
+        if (current_index := await self.get_first_lit_index()) is not None:
+            next_index = (current_index + 1) % len(self.ent_lights)
+        else:
+            next_index = 0
+
+        next_light = self.ent_lights[next_index]
+        self.logger.info('Switching light no. %d (%r).', next_index, next_light.entity_id)
+
+        for light in self.ent_lights:
+            if light is next_light:
+                self.logger.debug('Switching on %r.', light.entity_id)
+                await light.turn_on()
+            else:
+                self.logger.debug('Switching off %r.', light.entity_id)
+                await light.turn_off()
+
+
+    async def toggle_all_lights(self, entity, attribute, old, new, kwargs):
+        if await self.get_first_lit_index() is None:
+            service = 'turn_on'
+        else:
+            service = 'turn_off'
+
+        self.logger.info('Invoking %r for all lights.', service)
+
+        for light in self.ent_lights:
+            await light.call_service(service)
+
+    async def get_first_lit_index(self) -> int | None:
+        """Return index of first light that is lit or None."""
+        for index, light in enumerate(self.ent_lights):
+            if await light.get_state() == 'on':
+                return index
+
+        # no light is lit:
+        return None
